0c38d9264a7aa3b5e41933f9de01e4a0e8760098
Salmanul Fariz N <salmanulfariz997@gmail.com>
2022-12-03 19:28:35 +0530

    complete modal open function

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-'use strict';
+"use strict";
+
+const buttonOpenModal = document.querySelectorAll(".show-modal");
+const modal = document.querySelector(".modal");
+const buttonCloseModal = document.querySelector(".close-modal");
+const overlay = document.querySelector(".overlay");
+
+for (let i = 0; i < buttonOpenModal.length; i++) {
+  buttonOpenModal[i].addEventListener("click", () => {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+  });
+}
