cdf5e37b3a41c059057aa8e1b31a91646e6e5d9a
Salmanul Fariz N <salmanulfariz997@gmail.com>
2022-12-03 19:37:25 +0530

    complete modal close function

--- script.js
+++ script.js
@@ -5,9 +5,20 @@ const modal = document.querySelector(".modal");
 const buttonCloseModal = document.querySelector(".close-modal");
 const overlay = document.querySelector(".overlay");
 
+// Open Modal when button is click
 for (let i = 0; i < buttonOpenModal.length; i++) {
   buttonOpenModal[i].addEventListener("click", () => {
     modal.classList.remove("hidden");
     overlay.classList.remove("hidden");
   });
 }
+
+// Close Modal while click close button or outer of modal
+
+function closeModal() {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+}
+
+buttonCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
